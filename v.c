1e4077e3c98f70c7240a1e4a2e46557a2b8ff2d0
logitha <loga848.456@gmail.com>
2016-07-06 09:31:09 +0530

    Create v.c

---
+++ v.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdio.h>
+void main()
+{
+char a[15]={25525511135};
+int i,j=0,l;
+clrscr();
+scanf("%d",&a[i]);
+l=strlen(a);
+if(a[2]==5 && a[5]==5 && a[8]==1)
+{
+printf("%d",a[i]);
+}
+else
+{
+printf(".");
+j++;
+getch();
+}
+}
